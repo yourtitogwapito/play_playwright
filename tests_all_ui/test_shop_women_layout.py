bdc73fc0af557eba088f05f142f65ca760ea1a6c
yourtitogwapito <122256680+yourtitogwapito@users.noreply.github.com>
2023-02-12 13:53:03 +0800

    Update test_shop_women_layout.py

--- tests_all_ui/test_shop_women_layout.py
+++ tests_all_ui/test_shop_women_layout.py
@@ -13,8 +13,8 @@ import pytest, time
 def test_shop_women(ConfTest) -> None:
     page = ConfTest
     shop_women = ShopWomen(page)
-    time.sleep(3)
     page.locator("#comp-kqx7ocfp1").get_by_test_id("linkElement").click()
+    time.sleep(3)
     assert shop_women.shoes.is_visible()
     
     
