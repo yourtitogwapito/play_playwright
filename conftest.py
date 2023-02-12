85e7565d4986822fbcf2bc7b4a0222cd5f701968
yourtitogwapito <122256680+yourtitogwapito@users.noreply.github.com>
2023-02-12 15:41:12 +0800

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -14,14 +14,14 @@ from playwright.sync_api import Playwright, sync_playwright, expect
 import pytest
 
 @pytest.fixture
-def ConfTest(playwright: Playwright):
-    #page.goto("https://symonstorozhenko.wixsite.com/website-1")
-    #page.set_default_timeout(3000)
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    page = context.new_page()
+def ConfTest(page): #playwright: Playwright):
     page.goto("https://symonstorozhenko.wixsite.com/website-1")
     page.set_default_timeout(3000)
+    # browser = playwright.chromium.launch(headless=False)
+    # context = browser.new_context()
+    # page = context.new_page()
+    # page.goto("https://symonstorozhenko.wixsite.com/website-1")
+    # page.set_default_timeout(3000)
     yield page
     
     page.close()
