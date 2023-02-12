af49dbe7385971fd5fc105dc8f0f3c5fa017b524
yourtitogwapito <122256680+yourtitogwapito@users.noreply.github.com>
2023-02-12 16:00:02 +0800

    Update test_shop_women_layout.py

--- tests_all_ui/test_shop_women_layout.py
+++ tests_all_ui/test_shop_women_layout.py
@@ -13,6 +13,7 @@ import pytest, time
 def test_shop_women(ConfTest) -> None:
     page = ConfTest
     shop_women = ShopWomen(page)
+    time.sleep(3)
     page.locator("#comp-kqx7ocfp1").get_by_test_id("linkElement").click()
     time.sleep(3)
     assert shop_women.shoes.is_visible()
@@ -23,6 +24,7 @@ def test_shop_women(ConfTest) -> None:
 def test_shop_wome2n(ConfTest) -> None:
     page = ConfTest
     shop_women = ShopWomen(page)
+    time.sleep(3)
     page.locator("#comp-kqx7ocfp1").get_by_test_id("linkElement").click()
     time.sleep(3)
     assert shop_women.backpack.is_visible()
